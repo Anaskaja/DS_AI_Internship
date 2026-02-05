cabef537bc5012882702d265194a6e37fcd64a5b
Anaskaja <anas31kaja@gmail.com>
2026-02-05 15:46:15 +0530

    modified:   day 3/day3_collections.py         deleted:    day 4/day4_fundamentals.py                 day 3/The Immutable config         day 3/The inventory manager.py         day 3/data slicer.py         day 4/Task 1_The Personal Contact Book (Dictionaries)         day 4/Task 2_The Duplicate Cleaner (Sets)         day 4/Task 3_The Interest Matcher (Set Operations)         day 4/day4_dictionaries_sets.py

---
+++ Src/day 3/The inventory manager.py
@@ -0,0 +1,8 @@
+fruits =["Apples","Bananas","Carrots","Dates"]
+print(fruits)
+fruits.append("Eggs")
+print(fruits)
+fruits.remove("Bananas")
+print(fruits)
+fruits.sort()
+print(fruits)
---
+++ Src/day 3/data slicer.py
@@ -0,0 +1,7 @@
+temperatures = [22, 24, 25, 28, 30, 29, 27, 26, 24, 22]
+print(temperatures[0])
+print(temperatures[-1])
+temperatures[3:6]
+temperatures[-3]
+print(f"Afternoon peak: {temperatures[3:6]}")
+print(f"Last 3 hours: {temperatures[-3:]}")
